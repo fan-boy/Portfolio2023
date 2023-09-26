8158707952d2ef6587bc22ce3d25cfed8b791e4a
Aaditya Shete <fan-boy@users.noreply.github.com>
2023-09-26 16:55:16 -0400

    Responsive navbar

--- src/app/ComponentLibrary/Components/Navbar.tsx
+++ src/app/ComponentLibrary/Components/Navbar.tsx
@@ -8,11 +8,26 @@ interface NavbarProps{
 const Navbar = () =>{
 
     return(
-        <div className="w-full bg-[#E7E7E7] p-2  gap-2  flex flex-row justify-center">
+        <div className="w-full bg-[#E7E7E7] p-2   items-center sm:grid sm:grid-cols-3 ">
 
+            <span className="hidden sm:block font-sans text-fontLight ml-4">
+            <span>
+                Aaditya Shete
+            </span>
+            <span className="font-thin ml-2">
+                Portfolio
+            </span>
+            </span>
+
+            <div className=" flex items-center justify-center">
+
+            <span className="flex  flex-row gap-2 justify-self-center">
            <NavbarButton intent={"selected"}>Work</NavbarButton>
            <NavbarButton>About</NavbarButton>
            <NavbarButton>Resume</NavbarButton>
+           </span>
+           </div>
+           
         </div>
     );
 
