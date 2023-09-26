76d28a7b6a5a2acda22cd4eeaf90b2293d520a57
Aaditya Shete <fan-boy@users.noreply.github.com>
2023-09-26 15:08:47 -0400

    Spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Type, Variants } from './ComponentLibrary/Typography/Type'
 export default function Home() {
   return (
    <DefaultPage selected="work">
-     <div className="flex flex-col">
+     <div className="flex flex-col mt-14">
      <div className="flex mt-4 flex-col text-center gap-4">
        <span className="">
          Hi! I&#x27;m
