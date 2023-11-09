7a8d8d2dcabfab76071efe9304f9a521630dbdf2
Aaditya Shete <fan-boy@users.noreply.github.com>
2023-11-09 16:57:06 -0500

    About file

---
+++ src/app/about/page.tsx
@@ -0,0 +1,85 @@
+import Image from 'next/image'
+import React from 'react'
+import DefaultPage from '../ComponentLibrary/commons/pages/DefaultPage'
+import PageSection from '../ComponentLibrary/commons/pages/PageSection'
+import Card from '../ComponentLibrary/Components/Card/Card'
+import Footer from '../ComponentLibrary/Components/footer'
+import Navbar from '../ComponentLibrary/Components/Navbar'
+import BouncingArrow from '../ComponentLibrary/Specials/BouncingArrow'
+import { Type, Variants } from '../ComponentLibrary/Typography/Type'
+
+export default function Home() {
+  return (
+    <DefaultPage selected="work">
+      <PageSection theme="dark">
+      <div className="flex flex-col mt-14 mb-14">
+        <div className="md:flex md:flex-row md:items-end md:gap-10">
+          <div className="flex mt-4 flex-col text-center md:text-left  md:min-w-1/2 gap-4 md:gap-0">
+            <span>
+              <Type variant={"body"}>Hi! I&#x27;m</Type>
+            </span>
+            <span >
+              <Type variant={"heroText"}>
+                Aaditya
+              </Type>
+            </span>
+          </div>
+          <div className="flex mt-9 flex-col text-center md:text-left md:min-w-1/2">
+            <span>
+              <Type variant={"h3"}> A product designer </Type>
+            </span>
+            <span className="font-serif text-2xl">
+              <Type variant={"h3"}> who codes </Type>
+            </span>
+          </div>
+        </div>
+        <div className=" md:flex md:flex-row md:gap-12">
+          <div className="hidden md:flex mt-20 md:flex-col text-center md:text-left gap-4 md:min-w-1/2">
+            <span >
+              <Type variant={"body"}>
+                I empathize with users to <br /> bridge the gap between the <br /> intent and impact of a product
+                
+              </Type>
+            </span>
+          </div>
+          <div className="flex mt-20 flex-col text-center md:text-left gap-4 md:min-w-1/2">
+            <span >
+              <Type variant={"body"}>
+                HCIM grad student at <br /> University of Maryland, CP
+              </Type>
+            </span>
+            <span >
+              <Type variant={"body"}>
+                Graduating May 2024
+              </Type>
+            </span>
+          </div>
+        </div>
+        <div className="w-full text-center md:text-left mt-10 md:mt-28 md:flex md:flex-row-reverse">
+          <div className="flex flex-row gap-5">
+          <Type variant="emphasized-body">
+              Selected Work
+          </Type>
+          <BouncingArrow />
+          </div>
+        </div>
+      </div>
+      
+      </PageSection>
+      <PageSection theme="light"  >
+        <div className="w-full flex my-10 md:flex-wrap md:gap-5">
+          <div className="min-w-full w-full md:w-2/5 md:min-w-2/5 flex-1 ">
+         <Card title="Crashr" intent="primary"/>
+         </div>
+         <div className="min-w-2/5 flex-1 md:translate-y-1/2">
+         <Card title="Chain Reactive LLC" intent="primary"/>
+         </div>
+         <div className="min-w-2/5 w-1/2 ">
+         <Card title="Crashr" intent="primary"/>
+         </div>
+        </div>
+      </PageSection>
+      
+    </DefaultPage>
+  )
+}
