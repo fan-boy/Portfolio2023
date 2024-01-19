45913c4ff4061b75215a62f2dcb1839b69fddb24
Aaditya Shete <fan-boy@users.noreply.github.com>
2024-01-19 18:06:58 +0530

    Added lightbox

--- src/app/about/photoalbum.tsx
+++ src/app/about/photoalbum.tsx
@@ -3,6 +3,15 @@ import PhotoAlbum from "react-photo-album";
 import Image from "next/image";
 import type { RenderPhotoProps } from "react-photo-album";
 import photos from "./images";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+// import optional lightbox plugins
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import { useState } from "react";
+
 
 
 function NextJsImage({
@@ -10,6 +19,7 @@ function NextJsImage({
   imageProps: { alt, title, sizes, className, onClick },
   wrapperStyle,
 }: RenderPhotoProps) {
+    
   return (
     <div style={{ ...wrapperStyle, position: "relative" }}>
       <Image
@@ -28,9 +38,13 @@ function NextJsImage({
 
 
 export default function Gallery() {
-    return <PhotoAlbum layout="masonry" spacing={10} photos={photos}
+    const [index, setIndex] = useState(-1);
+    return (
+    <>
+    <PhotoAlbum layout="masonry" spacing={10} photos={photos}
     renderPhoto={NextJsImage}
       defaultContainerWidth={1200}
+      onClick={({ index }) => setIndex(index)}
       sizes={{
         size: "calc(100vw - 40px)",
         sizes: [
@@ -40,5 +54,15 @@ export default function Gallery() {
         ],
       }}
     
-    />;
+    />
+    <Lightbox
+        slides={photos}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        plugins={[ Slideshow, Thumbnails]}
+      />
+    </>
+    )
+    ;
 }
