1d113cf33bfccda0807090015c59cef1f9ba5336
Aaditya Shete <fan-boy@users.noreply.github.com>
2024-03-13 13:19:39 -0400

    Last update change

--- src/app/ComponentLibrary/Components/footer.tsx
+++ src/app/ComponentLibrary/Components/footer.tsx
@@ -53,7 +53,7 @@ const Footer = () => {
             </div>
             <div className=" flex flex-col md:flex-row  items-center justify-center">
                 <Type variant="body">
-                  Last updated Feb 2024
+                  Last updated March 2024
                 </Type>
             </div>
             </div>
