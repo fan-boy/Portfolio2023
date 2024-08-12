a26ce64222f3b082bf78f1a6b8a2037cd9982d40
Aaditya Shete <fan-boy@users.noreply.github.com>
2024-08-12 15:26:17 -0400

    Removed hive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -90,11 +90,7 @@ export default function Home() {
 
           <Card title={projectlist[1].projectName} type={projectlist[1].type as varianttype} subtitle={projectlist[1].subtitle} tags={projectlist[1].tags} />
           </div>
-          <div className="mt-10">
-
-          <Card title={projectlist[3].projectName} type={projectlist[3].type as varianttype} subtitle={projectlist[3].subtitle} tags={projectlist[3].tags} />
-          </div>
-          
+         
 
         </div>
         </PageSection.ConstrainedWidth>
